d8cfe2ad71bc239824269214b552bad06e292f58
Andrew Prock <drewster@Andrews-MacBook-Pro.local>
2013-05-26 18:49:26 -0700

    remove tabs

--- programs/pscolex/main.cpp
+++ programs/pscolex/main.cpp
@@ -18,11 +18,11 @@ int main (int argc, char ** argv)
     {
         // set up the program options, handle the help case, and extract the values
         po::options_description desc("pscolex, a utility which prints all combinations\n"
-									 "of poker hands, using canonical suits, or only ranks\n");
+                                     "of poker hands, using canonical suits, or only ranks\n");
         desc.add_options()
-            ("help,?",	  "produce help message")
+            ("help,?",    "produce help message")
             ("num-cards,n", po::value<size_t>()->default_value(2), "number of cards in hands")
-			("ranks",       "print the set of rank values")
+            ("ranks",       "print the set of rank values")
             ;
       
         po::variables_map vm;
@@ -40,34 +40,34 @@ int main (int argc, char ** argv)
         }
 
         // extract the options
-		size_t num_cards = vm["num-cards"].as<size_t>();
+        size_t num_cards = vm["num-cards"].as<size_t>();
 
-		set<CardSet> canonicalHands;
-		map<string,size_t> rankHands;
-		combinations cards(52,num_cards);
-		do
-		{
-			CardSet hand;
-			for (size_t i=0; i<num_cards; i++)
-			{
-				hand.insert (Card(cards[i]));
-			}
-			canonicalHands.insert(hand.canonize());
-			rankHands[hand.rankstr()] = hand.rankColex();
-		}
-		while (cards.next());
+        set<CardSet> canonicalHands;
+        map<string,size_t> rankHands;
+        combinations cards(52,num_cards);
+        do
+        {
+            CardSet hand;
+            for (size_t i=0; i<num_cards; i++)
+            {
+                hand.insert (Card(cards[i]));
+            }
+            canonicalHands.insert(hand.canonize());
+            rankHands[hand.rankstr()] = hand.rankColex();
+        }
+        while (cards.next());
 
-		if (vm.count("ranks") > 0)
-		{
-			for (auto it=rankHands.begin(); it != rankHands.end(); it++)
-				cout << boost::format("%s: %d\n") % it->first % it->second;
-		}
-		else
-		{
-			for (auto it=canonicalHands.begin(); it != canonicalHands.end(); it++)
-				cout << boost::format("%s: %d\n") % it->str() % it->colex();
-		}
-	}
+        if (vm.count("ranks") > 0)
+        {
+            for (auto it=rankHands.begin(); it != rankHands.end(); it++)
+                cout << boost::format("%s: %d\n") % it->first % it->second;
+        }
+        else
+        {
+            for (auto it=canonicalHands.begin(); it != canonicalHands.end(); it++)
+                cout << boost::format("%s: %d\n") % it->str() % it->colex();
+        }
+    }
     catch(std::exception& e) 
     {
         cerr << "-- caught exception--\n" << e.what() << "\n";
